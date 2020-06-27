98aeee834b0221697780fabdad3d797ccc217ac0
Omar M Juarez <omarmju906p@hotmail.com>
2020-06-26 20:56:48 -0500

    Se agrega el botón Mute.

--- website/assets/index.js
+++ website/assets/index.js
@@ -2,7 +2,9 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlayer from "./plugins/AutoPlayer.js";
 
 const video = document.querySelector("video");
-const boton = document.querySelector("button");
+const boton = document.getElementById("playButton");
+const botonMute = document.getElementById("muteButton");
 
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlayer()] });
 boton.onclick = () => player.togglePlay();
+botonMute.onclick = () => player.toggleMute();
