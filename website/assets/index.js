de34f8e1eadb0699d7b9f223cdb31deee930a7c9
Omar M Juarez <omarmju906p@hotmail.com>
2020-06-26 20:49:28 -0500

    Se agrega plugin de Autoplay.

--- website/assets/index.js
+++ website/assets/index.js
@@ -1,7 +1,8 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlayer from "./plugins/AutoPlayer.js";
 
 const video = document.querySelector("video");
 const boton = document.querySelector("button");
 
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlayer()] });
 boton.onclick = () => player.togglePlay();
