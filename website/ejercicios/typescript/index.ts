1d4a70895fa0d7f83b92ca39e53b64fcbceea090
Omar M Juarez <omarmju906p@hotmail.com>
2020-07-04 17:48:21 -0500

    Se cambia de servidor LiveServer a Parcel para comensar con TypeScript.

--- website/ejercicios/typescript/index.ts
+++ website/ejercicios/typescript/index.ts
@@ -1,30 +1,50 @@
-// Interfaces
-enum Color {
-  Rojo = 'Rojo',
-  Verde = 'Verde',
+console.log("Hola TypeScript");
+
+function add(a: number, b: number) {
+  return a + b;
 }
 
-interface Rectangulo {
-  ancho: number;
-  alto: number;
-  color?: Color;
+const result = add(2, 4);
+
+// Arreglos.
+let people: string[] = [];
+people = ["Omar", "Daniel", "Lesly", "Jiemena", "Tania"];
+people.push("Valeria");
+console.log(people);
+
+let peopleAndAge: Array<string | number> = [];
+peopleAndAge = ["Omar", 29, "Daniel", 25, "Lesly", 23, "Jiemena", 22, "Tania", 18];
+peopleAndAge.push("Sharon");
+peopleAndAge.push(27);
+console.log(peopleAndAge);
+
+// Enums.
+enum Colores {
+  Rojo = "Rojo",
+  Azul = "Azul",
+  Amarillo = "Amarillo",
+  Verde = "Verde"
 }
 
-let rect: Rectangulo = {
-  ancho: 4,
-  alto: 6,
-  // color: Color.Rojo,
-};
+const colorFavorito = Colores.Azul;
+console.log(`Mi color favorito es el ${colorFavorito}`);
 
-function area(r: Rectangulo): number {
-  return r.alto * r.ancho;
-}
+// Any.
+let comodin: any = "Jocker";
+comodin = { type: "WildCart" }
 
-const areaRect = area(rect);
-console.log(areaRect);
+// Objects.
+let someObject: object = { type: "Objeto" }
 
-rect.toString = function() {
-  return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
-};
-
-console.log(rect.toString());
+/**
+ * Tipos de datos en TS
+ * Tipos:
+    boolean. Valor verdadero o falso.
+    number. Números.
+    string. Cadenas de texto.
+    string[]. Arreglo del tipo cadena de texto.
+    Array. Arreglo multi-tipo, acepta cadenas de texto o números.
+    enum. Es un tipo especial llamado enumeración.
+    any. Cualquier tipo.
+    object. Del tipo objeto.
+ */
