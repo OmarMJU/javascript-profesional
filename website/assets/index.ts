75dde2fa66cf7ce1d3be8a273d58eeb4c8e7a121
Omar M Juarez <omarmju906p@hotmail.com>
2020-07-09 19:17:54 -0500

    Se agrega el plugin Ads.

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlayer from "./plugins/AutoPlayer";
 import AutoPause from "./plugins/AutoPause";
+import Ads from "./plugins/Ads";
 
 const video = document.querySelector("video");
 const boton: HTMLElement = document.getElementById("playButton");
@@ -8,7 +9,7 @@ const botonMute: HTMLElement = document.getElementById("muteButton");
 
 const player = new MediaPlayer({
     el: video,
-    plugins: [new AutoPlayer(), new AutoPause()]
+    plugins: [new AutoPlayer(), new AutoPause(), new Ads()]
 });
 boton.onclick = () => player.togglePlay();
 botonMute.onclick = () => player.toggleMute();
